c378abaaafcb4025ff76b2c5159df7e3c8aacfbf
SY3D <syedhamzatahir1001@gmail.com>
2024-05-01 18:05:29 +0500

    Added bleu score

---
+++ TextSummarization/BleuScore.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from datasets import load_metric
+bleu_metric = load_metric("sacrebleu")
+bleu_metric.add(
+ prediction="the the the the the the", reference=["the cat is on the mat"])
+results = bleu_metric.compute(smooth_method="floor", smooth_value=0)
+results["precisions"] = [np.round(p,2) for p in results["precisions"]]
+pd.DataFrame.from_dict(results, orient="index",columns=["Value"])
